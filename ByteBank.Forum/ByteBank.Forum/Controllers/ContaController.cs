b46e86b925f0239a0a0be6bef1907f69a5b4b111
GabrielRioo <gabrielpaivario@gmail.com>
2020-12-19 19:53:23 -0300

    created the connection with database and user registed working

--- ByteBank.Forum/ByteBank.Forum/Controllers/ContaController.cs
+++ ByteBank.Forum/ByteBank.Forum/Controllers/ContaController.cs
@@ -1,7 +1,11 @@
-﻿using ByteBank.Forum.ViewModels;
+﻿using ByteBank.Forum.Models;
+using ByteBank.Forum.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,14 +19,30 @@ namespace ByteBank.Forum.Controllers
         }
 
         [HttpPost]
-        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        public async Task<ActionResult> Registrar(ContaRegistrarViewModel modelo)
         {
             if(ModelState.IsValid) //Valida se o campo obrigatorio foi preenchido
             {
+                //Cria a conecxao com dbContext
+                var dbContext = new IdentityDbContext<UsuarioAplicacao>("DefaultConnection");
+                // Camada q conversa com nosso Db Context -> IuserStore
+                var userStore = new UserStore<UsuarioAplicacao>(dbContext);
+                var userManager = new UserManager<UsuarioAplicacao>(userStore);
+                var novoUsuario = new UsuarioAplicacao();
+
+                novoUsuario.Email = modelo.Email;
+                novoUsuario.UserName = modelo.UserName;
+                novoUsuario.NomeCompleto = modelo.NomeCompleto;
+
+                // Criar o novo usuario
+                await userManager.CreateAsync(novoUsuario, modelo.Senha);
+
                 //Podemos incluir o usuário
                 return RedirectToAction("Index", "Home");
             }
-            return View();
+
+            //Algo de errado aconteceu
+            return View(modelo);
         }
     }
 }
